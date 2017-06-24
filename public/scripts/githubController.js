4c1031a3ae43964edaa4e586030ec4b9c3e143b1
Kara Bayse <karabayse@gmail.com>
2017-06-24 12:53:57 -0500

    Server up and running

---
+++ public/scripts/githubController.js
@@ -0,0 +1,11 @@
+/*---- Controller ----*/
+
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.controller('GitHubController', function(GitHubService){
+  console.log('in GitHubController');
+  var vm = this;
+
+
+
+}); // end controller
